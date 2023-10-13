9ad4c6ba844fad4cc71705f65fa4be12c7404a51
mvcfvsgdj <coczzzz@naver.com>
2023-10-13 17:45:36 +0900

    23/10/13

--- dbtest/src/main/java/db/test/api/APIController.java
+++ dbtest/src/main/java/db/test/api/APIController.java
@@ -1,10 +1,15 @@
 package db.test.api;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,7 +24,20 @@ public class APIController {
 		String result = api.getApiTest();
 		return result;
 	}
+
+	@ResponseBody
+	@GetMapping(value = "/airList")
+	public ArrayList<AirDTO> airAPI() throws IOException {
+		String result = api.getApiTest();
+		ArrayList<AirDTO> list = api.fromJSONItems(result);
+		return list;
+	}
+
+	@RequestMapping(value = "/airCheck", method = RequestMethod.GET)
+	public void SearchAll2(Model model) throws SQLException, IOException {
+		String result = api.getApiTest();
+		ArrayList<AirDTO> list = api.fromJSONItems(result);		
+		model.addAttribute("list", list);
+
+	}
 }
-//"informCause"
-//"dataTime"
-//"informGrade"
--- dbtest/src/main/java/db/test/api/ApiExplorer.java
+++ dbtest/src/main/java/db/test/api/ApiExplorer.java
@@ -1,14 +1,16 @@
 package db.test.api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
-
-import java.io.BufferedReader;
-import java.io.IOException;
 
 @Component
 public class ApiExplorer {
@@ -20,8 +22,8 @@ public class ApiExplorer {
 																														 * Service
 																														 * Key
 																														 */
-		urlBuilder.append(
-				"&" + URLEncoder.encode("returnType", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /* xml 또는 json */
+		urlBuilder.append("&" + URLEncoder.encode("returnType", "UTF-8") + "="
+				+ URLEncoder.encode("json", "UTF-8")); /* xml 또는 json */
 		urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "="
 				+ URLEncoder.encode("100", "UTF-8")); /* 한 페이지 결과 수(조회 날짜로 검색 시 사용 안함) */
 		urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "="
@@ -30,6 +32,7 @@ public class ApiExplorer {
 				+ URLEncoder.encode("2023-10-10", "UTF-8")); /* 통보시간 검색(조회 날짜 입력이 없을 경우 한달동안 예보통보 발령 날짜의 리스트 정보를 확인) */
 		urlBuilder.append("&" + URLEncoder.encode("InformCode", "UTF-8") + "="
 				+ URLEncoder.encode("PM10", "UTF-8")); /* 통보코드검색(PM10, PM25, O3) */
+
 		URL url = new URL(urlBuilder.toString());
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
@@ -51,4 +54,45 @@ public class ApiExplorer {
 		String result = sb.toString();
 		return result;
 	}
+
+	public ArrayList<AirDTO> fromJSONItems(String result) {
+		ArrayList<AirDTO> airList = new ArrayList<>();
+		JSONObject jsonResult = new JSONObject(result);
+		JSONObject response = jsonResult.getJSONObject("response");
+		JSONObject body = response.getJSONObject("body");
+		JSONArray items = body.getJSONArray("items");
+
+
+		for (int i = 0; i < items.length(); i++) {
+			JSONObject item = items.getJSONObject(i);
+
+			// "informCause", "dataTime", "informGrade" 값을 추출하여 AirDTO 객체 생성
+			String informCause = item.getString("informCause");
+			String dataTime = item.getString("dataTime");
+			String informGrade = item.getString("informGrade");
+			String img  =  String.valueOf(item.get("imageUrl1"));   // null  -> "null" 
+			String img2  =  String.valueOf(item.get("imageUrl2"));   // null  -> "null" 
+			String img3  =  String.valueOf(item.get("imageUrl3"));   // null  -> "null" 
+			String img4  =  String.valueOf(item.get("imageUrl4"));   // null  -> "null" 
+			String img5  =  String.valueOf(item.get("imageUrl5"));   // null  -> "null" 
+			String img6  =  String.valueOf(item.get("imageUrl6"));   // null  -> "null" 
+			
+
+			AirDTO airDTO = new AirDTO();
+			airDTO.setInformCause(informCause);
+			airDTO.setDataTime(dataTime);
+			airDTO.setInformGrade(informGrade);
+			airDTO.setImg(img);
+			airDTO.setImg2(img2);
+			airDTO.setImg3(img3);
+			airDTO.setImg4(img4);
+			airDTO.setImg5(img5);
+			airDTO.setImg6(img6);
+
+			airList.add(airDTO);
+		}
+
+		return airList;
+	}
+
 }
